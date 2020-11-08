7f0651934747b785b8d33a632a2ffebe3c4156d2
Joelle Kado <jkado@my.hpu.edu>
2020-11-08 14:34:20 -0600

    imported images and relocated files

--- scripts/client.js
+++ scripts/client.js
@@ -52,11 +52,18 @@ function addEmployee() {//ENTER addEmployee
     appendMonthlyTODOM();
 };//EXIT addEmployee 
 
+function financial(x) {
+    return Number.parseFloat(x).toFixed(2);
+  }
+  
+  console.log(financial(123.456));
+  // expected output: "123.46"
+  
 function appendMonthlyTODOM() {//ENTER appendMonthlyTODOM
     //clear DOM
     $('#monthlyCompanyCostToDOM').empty();
     //append monthly cost to DOM.
-    $('#monthlyCompanyCostToDOM').append(`Monthly Company Cost: $${totalCompanyCost / 12}`);
+    $('#monthlyCompanyCostToDOM').append(`Monthly Company Cost: $${Number.parseFloat(totalCompanyCost / 12).toFixed(2)}`);
     //clear the input fields
     $('#firstNameIn').val('');
     $('#lastNameIn').val('');
