708802c6131f6a43379e9b2774d34650c7608702
Joelle Kado <jkado@my.hpu.edu>
2020-11-07 21:59:05 -0600

    employeeRoladex no longer needed

--- client.js
+++ client.js
@@ -11,19 +11,13 @@ function readyNow() {//ENTER readyNow
 };//EXIT readyNow  
 
 let totalCompanyCost= 0;
+//let employeeRoladex = []; 
 
-
-let employeeRoladex = []; 
-//let monthlyCompanyCost = totalCompanyCost / 12;
-
-function addEmployee() {//ENTER addEmployee
-   // let monthlyCompanyCost = totalCompanyCost / 12;
-    
+function addEmployee() {//ENTER addEmployee    
     //use the event passed in to stop the default action
     //of page refresh
     event.preventDefault(event);
     
-
     let employeeObject = {
         firstName : $('#firstNameIn').val(),
         lastName : $('#lastNameIn').val(),
@@ -32,43 +26,32 @@ function addEmployee() {//ENTER addEmployee
         annualSalary : $('#annualSalaryIn').val()
     }
     totalCompanyCost += Number(employeeObject.annualSalary);
-    employeeRoladex.push(employeeObject)
+    //employeeRoladex.push(employeeObject)
     console.log(employeeObject);
-    console.log(employeeRoladex);
+    //console.log(employeeRoladex);
     console.log(`The 'totalCompanyCost' is $${totalCompanyCost} 
     And, The 'MonthlyCompanyCost' is $${totalCompanyCost / 12}`);
     
-    
     //Append employee info to the DOM 
-let firstNameToDOM = `<th class="">${employeeObject.firstName}</th>`;
-let lastNameToDOM = `<th class="">${employeeObject.lastName}</th>`;
-let iDNumToDOM = `<th class="">${employeeObject.iDNumber}</th>`;
-let jobTitleToDOM = `<th class="">${employeeObject.jobTitle}</th>`;
-let annualSalaryTODOM = `<th class="">${employeeObject.annualSalary}</th>`;
-$('#employeeTable').append(`<tr>${firstNameToDOM}, ${lastNameToDOM}, ${iDNumToDOM}, ${jobTitleToDOM}, ${annualSalaryTODOM}</tr>`);
+    let firstNameToDOM = `<th class="">${employeeObject.firstName}</th>`;
+    let lastNameToDOM = `<th class="">${employeeObject.lastName}</th>`;
+    let iDNumToDOM = `<th class="">${employeeObject.iDNumber}</th>`;
+    let jobTitleToDOM = `<th class="">${employeeObject.jobTitle}</th>`;
+    let annualSalaryTODOM = `<th class="">${employeeObject.annualSalary}</th>`;
+    $('#employeeTable').append(`<tr>${firstNameToDOM}, ${lastNameToDOM}, ${iDNumToDOM}, ${jobTitleToDOM}, ${annualSalaryTODOM}</tr>`);
 
-   
-   appendMonthlyTODOM();
-    
-
-
-   // calculateMonthlyCost();
+    appendMonthlyTODOM();
 };//EXIT addEmployee 
 
-
 function appendMonthlyTODOM( ) {//ENTER appendMonthlyTODOM
-//clear DOm
-$('#monthlyCompanyCostToDOM').empty();
-//append monthly cost to DOM.
-$('#monthlyCompanyCostToDOM').append(`Monthly Company Cost: $${totalCompanyCost / 12}`);
-//clear the input fields
-$('#firstNameIn').val('');
-$('#lastNameIn').val('');
-$('#iDNumIn').val('');
-$('#jobTitleIn').val('');
-$('#annualSalaryIn').val('');
-};//EXIT appendMonthlyTODOM
-//function calculateMonthlyCost() {//ENTER calculateMonthlyCost
-//console.log('In CalculateMonthlyCost');
-
-//};//EXIT calculateMonthlyCost
+    //clear DOM
+    $('#monthlyCompanyCostToDOM').empty();
+    //append monthly cost to DOM.
+    $('#monthlyCompanyCostToDOM').append(`Monthly Company Cost: $${totalCompanyCost / 12}`);
+    //clear the input fields
+    $('#firstNameIn').val('');
+    $('#lastNameIn').val('');
+    $('#iDNumIn').val('');
+    $('#jobTitleIn').val('');
+    $('#annualSalaryIn').val('');
+    };//EXIT appendMonthlyTODOM
